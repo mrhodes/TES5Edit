6853a2cf652b7bb1e33d3dbc74d1ad473d794147
Sharlikran <danielhmpdx16@gmail.com>
2015-11-17 13:23:26 -0800

    Updated IDLE

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -10220,7 +10220,8 @@ begin
       ], True)),
       wbInteger('Animation Group Section', itU8{, wbIdleAnam}),
       wbInteger('Replay Delay', itU16)
-    ], cpIgnore, True)
+    ], cpIgnore, True),
+    wbString(GNAM, 'Unknown')
   ]);
 
   wbRecord(INFO, 'Dialog response',
