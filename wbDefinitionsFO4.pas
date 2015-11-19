0d828084168e62602634e9ea52f5262bd2adad37
zilav <zilav18@gmail.com>
2015-11-19 14:23:26 -0800

    New Fallout4 Record KSSM

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -13515,7 +13515,16 @@ begin
   ]);
 
   wbRecord(KSSM, 'KSSM', [
-    wbEDID
+    wbEDID,
+    wbFormIDCk(DNAM, 'Sound', [SNDR]),
+    wbFormIDCk(ENAM, 'Sound', [SNDR]),
+    wbFormIDCk(VNAM, 'Sound', [SNDR]),
+    wbFloat(TNAM),
+    wbRArray('Keywords', wbFormIDCk(KNAM, 'Keyword', [KYWD])),
+    wbRArray('Sounds', wbStruct(RNAM, 'Sound', [
+      wbInteger('Unknown', itU32),
+      wbFormIDCk('Sound', [SNDR])
+    ]))
   ]);
 
   wbRecord(LAYR, 'LAYR', [
