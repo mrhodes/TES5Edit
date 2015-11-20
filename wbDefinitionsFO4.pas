586399a73d7a786a352708f4c795dd5802544bf7
zilav <zilav18@gmail.com>
2015-11-19 16:24:45 -0800

    New Fallout4 Record NOCM

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -13570,7 +13570,14 @@ begin
   ]);
 
   wbRecord(NOCM, 'NOCM', [
-    wbEDID
+    wbRArray('Unknown',
+      wbRStruct('Unknown', [
+        wbInteger(INDX, 'Index', itU32),
+        wbRArray('Unknown', wbUnknown(DATA)),
+        wbUnknown(INTV),
+        wbString(NAM1, 'Model')
+      ], [])
+    )
   ]);
 
 end;
